59eb708b86e2967a606dadc30ebc7c776732aeb8
itdanjalog <itdanja@kakao.com>
2025-01-23 20:45:03 +0900

    day68

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -27,15 +27,30 @@ const getLoginMid = ( ) =>{
                 console.log( '로그인상태');
                 //3. 로그아웃 버튼 , 마이페이지 버튼 , 로그인된 아이디 활성화 
                 html +=`<li class="nav-item">  <a class="nav-link" href="#"> ${ data } 님 </a> </li>
-                        <li class="nav-item">  <a class="nav-link" href="#"> 로그아웃 </a> </li>
+                        <li class="nav-item">  <a class="nav-link" href="#" onclick="logOut()"> 로그아웃 </a> </li>
                         <li class="nav-item">  <a class="nav-link" href="/member/login"> 마이페이지 </a> </li>`
             }
-
             // 4. 출력하기 
             memberBox.innerHTML = html;
-
         })
         .catch( error => { console.log( error)})
-
 } // f end 
 getLoginMid(); // JS 실행될때. 로그인 정보 요청 함수 호출 
+
+// [2] 로그아웃 함수
+const logOut = ( ) => {
+    // 1. fetch option
+    const option = { method : 'GET' }
+    // 2. fetch
+    fetch( '/member/logout.do' , option )
+        .then( response => response.json() )
+        .then( data => {
+            // 만약에 로그아웃 성공 했다면 로그인 페이지로 이동
+            if( data == true ){ alert('로그아웃 했습니다.'); location.href="/member/login"; } 
+        })
+        .catch( e => { console.log(e); })
+} // f end
+
+
+
+
