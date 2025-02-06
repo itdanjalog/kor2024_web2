a29989402b3b990a7bc27cb3564f2c4fdb6dbb8d
itdanjalog <itdanja@kakao.com>
2025-02-06 20:25:13 +0900

    day74

--- src/main/java/korweb/model/dto/BoardDto.java
+++ src/main/java/korweb/model/dto/BoardDto.java
@@ -1,13 +1,36 @@
 package korweb.model.dto;
 
+import korweb.model.entity.BoardEntity;
 import lombok.*;
 
-@Getter @Setter @ToString @Builder // 룸복
-@AllArgsConstructor @NoArgsConstructor // 룸복
+@Getter
+@Setter
+@ToString
+@Builder // 룸복
+@AllArgsConstructor
+@NoArgsConstructor // 룸복
 public class BoardDto {
-    // 코드 작성하기 전
-    private int bno;
-    private String btitle;
-    private String bcontent;
-    private int cno;
+    private int bno;            // 번호
+    private String btitle;      // 제목
+    private String bcontent;    // 내용
+    private int bview;          // 조회수
+    private int mno;            // 작성자의 회원번호
+    private int cno;            // 카테고리번호
+    private String cdate;       // 작성일
+
+    // 화면에는 작성자의 회원번호가 아닌 아이디를 출력해야 하므로
+    private String mid; // 작성자의 회원 아이디
+    private String cname; // 카테고리명
+
+    // + Dto --> entity 변환 메소드
+    // dto 를 entity 객체로 변환해서 데이터베이스에 저장 해야 하므로 변환이 필요하다.
+    public BoardEntity toEntity() {
+        return BoardEntity.builder()
+                .btitle(this.btitle)
+                .bcontent(this.bcontent)
+                .bview(this.bview)
+                .build();
+    }
+
 }
+
--- src/main/java/korweb/model/entity/BoardEntity.java
+++ src/main/java/korweb/model/entity/BoardEntity.java
@@ -1,6 +1,7 @@
 package korweb.model.entity;
 
 import jakarta.persistence.*;
+import korweb.model.dto.BoardDto;
 import lombok.*;
 
 @Getter @Setter @ToString @Builder // 룸복
@@ -31,4 +32,33 @@ public class BoardEntity extends BaseTime {
     @ManyToOne( cascade = CascadeType.ALL )
     @JoinColumn( name = "cno")
     private CategoryEntity categoryEntity;
+
+    // + entity --> Dto 변환 메소드
+    // 데이터베이스에 저장된 entity 를 조회한 후 dto로 변환해야 하므로 필요하다.
+    public BoardDto toDto(){
+        return BoardDto.builder()
+                .bno( this.bno )
+                .btitle( this.btitle )
+                .bcontent( this.bcontent )
+                .bview( this.bview )
+                .mno( this.memberEntity.getMno() )
+                .cno( this.categoryEntity.getCno() )
+                .mid( this.memberEntity.getMid() )
+                .cname( this.categoryEntity.getCname() )
+                .cdate( this.getCdate().toString() )
+                .build();
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
