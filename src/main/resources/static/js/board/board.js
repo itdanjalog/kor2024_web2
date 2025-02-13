340b8af43402b8570258373d966061b4890f20ff
itdanjalog <itdanja@kakao.com>
2025-02-13 19:40:51 +0900

    day78

--- src/main/resources/static/js/board/board.js
+++ src/main/resources/static/js/board/board.js
@@ -21,15 +21,18 @@ const findAll = ( ) => {
     // 3. fetch + 페이징번호
     fetch( `/board/findall.do?cno=${ cno }&page=${ page }` , option )
         .then( r => r.json() )
-        .then( data => {
+        .then( response => {
             // 4. 요청 결과 응답 자료 확인
-            console.log( data );
+            console.log( response );
             // 5. html를 출력할 구역 dom 가져오기
             const tbody = document.querySelector('tbody')
             // 6. 출력할 html를 저장하는 변수 선언
             let html = ``
             // 7. 응답 자료를 반복문 이용하여 하나씩 순회해서 html 누적으로 더해주기
-            data.forEach( board =>{
+                // + 응답 자료 에서 조회한 게시물 리스트 꺼내기
+                // response = { data : [] , page :  , totlapage : ···· }
+            let boardList = response.data;
+            boardList.forEach( board =>{
                 html += `<tr>
                             <td> ${ board.bno } </td>
                             <td> <a href="/board/view?bno=${ board.bno }"> ${ board.btitle } </a> </td>
@@ -40,7 +43,64 @@ const findAll = ( ) => {
             }) // f end
             // 8. 반복문 종료후 html 변수에 누적된 <tr> 출력하기
             tbody.innerHTML = html;
+
+            // 9. 게시물 출력후 페이징 버튼 생성 함수 호출
+            printPageNation( response , cno )
         })
         .catch( e => { console.log( e ); } )
 } // f end
 findAll(); // JS가 실행될 때 함수 실행
+
+// [2] 페이징 버튼 생성하는 함수 정의 ,
+const printPageNation = ( response , cno ) => {
+    let page = response.page;   // 현재페이지
+    let totalpage = response.totalpage // 전체페이지
+    let startbtn = response.startbtn // 현재 페이지의 페이징 버튼 시작번호
+    let endbtn = response.endbtn // 현재페이지의 페이징 버튼 끝번호
+    // (1) 어디에
+    const pagebox = document.querySelector('.pagebox')
+    // (2) 무엇을
+    let html = ``
+
+    // 이전 버튼 , 현재페이지 에서 - 1 차감한 페이지 이동 , 만약에 현재페이지가 1 이하 이면 1 고정 , 아니면 - 1
+    html += `<li class="page-item"><a class="page-link" href="/board?cno=${ cno }&page=${ page <= 1 ? 1 : page-1  }">이전</a></li>`
+
+    // 페이징 버튼 , 반복문 이용하여 statbtn 부터 endbtn 까지 페이징버튼 만들기
+    for( let index = startbtn ; index <= endbtn ; index++ ){
+        // 만약에 현재 페이지와 버튼번호가 같다면 .active 부트스트랩 클래스 부여
+        html += `<li class="page-item">
+                    <a class="page-link ${ page == index ? 'active' : '' }" href="/board?cno=${ cno }&page=${ index }">
+                        ${ index }
+                    </a>
+                </li>`
+    } // f end
+
+    // 다음 버튼 , 현재페이지 에서 + 1 증가한 페이지 이동 , 만약에 현재페이지가 전체페이지수 이상 이면 전체페이지수 고정 , 아니면 +1
+    html += `<li class="page-item"><a class="page-link" href="/board?cno=${ cno }&page=${ page >= totalpage ? totalpage : page +1 }">다음</a></li>`
+
+    // (3) 출력
+    pagebox.innerHTML = html;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
