4e5af3c3545a4570c98f31f47bc6a686b0c26a60
itdanjalog <itdanja@kakao.com>
2025-02-14 19:47:58 +0900

    day79

--- src/main/resources/static/js/board/board.js
+++ src/main/resources/static/js/board/board.js
@@ -15,11 +15,16 @@ const findAll = ( ) => {
     // 1. 현재 페이지 URL 에서 매개변수 page(페이지번호) 값 구하기.
     let page = new URL( location.href ).searchParams.get('page');
     if( page == null) page = 1; // 만약에 page가 없으면 1페이지로 설정
+    // 1. 검색필드 와 검색어
+    let key = new URL( location.href ).searchParams.get('key')
+    if( key == null ) key = ''; // 만약에 key가 없으면 공백으로 설정
+    let keyword = new URL( location.href ).searchParams.get('keyword')
+    if( keyword == null ) keyword = ''; // 만약에 keyword가 없으면 공백으로 설정
 
     // 2. fetch option
     const option = { method : 'GET' }
     // 3. fetch + 페이징번호
-    fetch( `/board/findall.do?cno=${ cno }&page=${ page }` , option )
+    fetch( `/board/findall.do?cno=${ cno }&page=${ page }&key=${ key }&keyword=${ keyword }` , option )
         .then( r => r.json() )
         .then( response => {
             // 4. 요청 결과 응답 자료 확인
@@ -45,14 +50,14 @@ const findAll = ( ) => {
             tbody.innerHTML = html;
 
             // 9. 게시물 출력후 페이징 버튼 생성 함수 호출
-            printPageNation( response , cno )
+            printPageNation( response , cno , key , keyword )
         })
         .catch( e => { console.log( e ); } )
 } // f end
 findAll(); // JS가 실행될 때 함수 실행
 
 // [2] 페이징 버튼 생성하는 함수 정의 ,
-const printPageNation = ( response , cno ) => {
+const printPageNation = ( response , cno , key , keyword ) => {
     let page = response.page;   // 현재페이지
     let totalpage = response.totalpage // 전체페이지
     let startbtn = response.startbtn // 현재 페이지의 페이징 버튼 시작번호
@@ -61,26 +66,45 @@ const printPageNation = ( response , cno ) => {
     const pagebox = document.querySelector('.pagebox')
     // (2) 무엇을
     let html = ``
-
     // 이전 버튼 , 현재페이지 에서 - 1 차감한 페이지 이동 , 만약에 현재페이지가 1 이하 이면 1 고정 , 아니면 - 1
-    html += `<li class="page-item"><a class="page-link" href="/board?cno=${ cno }&page=${ page <= 1 ? 1 : page-1  }">이전</a></li>`
-
+    html += `<li class="page-item">
+                <a class="page-link" href="/board?cno=${ cno }&page=${ page <= 1 ? 1 : page-1  }&key=${ key }&keyword=${ keyword }">
+                    이전
+                </a>
+            </li>`
     // 페이징 버튼 , 반복문 이용하여 statbtn 부터 endbtn 까지 페이징버튼 만들기
     for( let index = startbtn ; index <= endbtn ; index++ ){
         // 만약에 현재 페이지와 버튼번호가 같다면 .active 부트스트랩 클래스 부여
         html += `<li class="page-item">
-                    <a class="page-link ${ page == index ? 'active' : '' }" href="/board?cno=${ cno }&page=${ index }">
+                    <a class="page-link ${ page == index ? 'active' : '' }" href="/board?cno=${ cno }&page=${ index }&key=${ key }&keyword=${ keyword }">
                         ${ index }
                     </a>
                 </li>`
     } // f end
 
     // 다음 버튼 , 현재페이지 에서 + 1 증가한 페이지 이동 , 만약에 현재페이지가 전체페이지수 이상 이면 전체페이지수 고정 , 아니면 +1
-    html += `<li class="page-item"><a class="page-link" href="/board?cno=${ cno }&page=${ page >= totalpage ? totalpage : page +1 }">다음</a></li>`
+    html += `<li class="page-item">
+                <a class="page-link" href="/board?cno=${ cno }&page=${ page >= totalpage ? totalpage : page +1 }&key=${ key }&keyword=${ keyword }">
+                    다음
+                </a>
+            </li>`
 
     // (3) 출력
     pagebox.innerHTML = html;
+} // f end
+
+// [3] 검색 버튼을 클릭 했을때 함수
+const onSearch = ( ) => {
+    // 1. 선택한 검색필드 와 입력받은 검색어 가져오기
+    const key = document.querySelector('.key').value
+    const keyword = document.querySelector('.keyword').value
+    // 2. 현재 카테고리 번호 를 URL 에서 가져오기  // 검색 기능 실행시 현재 카테고리를 유지 하기 위해
+    const cno = new URL( location.href ).searchParams.get('cno');
+    // 3. 입력받은 검색필드 와 입력받은 검색어로 이동한다. cno는 유지 , 검색후 결과는 1페이지 설정
+    location.href = `board?cno=${ cno }&page=1&key=${ key }&keyword=${ keyword }`
 }
+
+
 
 
 
